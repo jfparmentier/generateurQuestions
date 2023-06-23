4c14cbe26a324c1cec132bdef31e2d7dceee9c6b
jfparmentier <jfparmentier@gmail.com>
2023-06-23 16:22:58 +0200

    debut git

---
+++ scripts/generationQR.js
@@ -0,0 +1,198 @@
+var OPENAI_API_KEY = "";
+var url_GPT = 'https://api.openai.com/v1/chat/completions';
+var model_GPT = "gpt-3.5-turbo-16k"; // alternative : "gpt-3.5-turbo"
+
+var listeQuestionsTypes = ["Que signifie ... ?",
+    "Quelle est la cause de ... ?", 
+    "Décrivez ... avec vos propres mots.",
+    "Expliquez pourquoi . . .",
+    "Comment ... pourrait-il affecter ... ?",
+    "Donnez un nouvel exemple de . . . ?",
+    "Comment utiliseriez-vous ... pour... ?",
+    "A votre avis, que se passerait-il si . . . ?",
+    "Comment . . . pourrait-il affecter . . . ?"];
+
+var listeQuestions = ["Que signifie l'équilibre thermodynamique ?", "A votre avis, que se passerait-il si l'on chauffait un gaz à volume constant ?"];
+var listeQuestionsReponses = [];
+
+
+function resume_PDF() {
+    afficheVue("resume_en_cours"); // si on regénère le résumé
+
+    var oHttp = new XMLHttpRequest();
+    oHttp.open("POST", url_GPT);
+    oHttp.setRequestHeader("Accept", "application/json");
+    oHttp.setRequestHeader("Content-Type", "application/json");
+    oHttp.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY)
+
+    oHttp.onreadystatechange = function () {
+        if (oHttp.readyState === 4) {                    
+            var oJson = {};
+            try {
+                oJson = JSON.parse(oHttp.responseText);
+            } catch (ex) {
+                console.log("Error: " + ex.message);
+            }
+            oJson = JSON.parse(oHttp.responseText);
+            var reponseGPT_str = oJson.choices[0].message.content;
+            document.getElementById("resumeCours").value = reponseGPT_str;            
+            afficheVue("validationResume");
+        }
+    }
+
+    var prompt = "Voici le contenu d'un cours : \n\n";
+    prompt += texte_pdf;
+    prompt += "\n\nQuelles sont les 7 idées principales de ce cours ? Explique chacune de celles-ci en trois lignes.";    
+    //console.log(prompt);
+
+    data = {        
+        model: model_GPT,
+        temperature: 0.7,
+        messages: [
+            {
+                "role": "system",
+                "content": "Tu es un enseignant qui fait réfléchir en profondeur aux concepts que tu enseignes."
+            },
+            {
+                "role": "user", //system,user,assistant
+                "content": prompt
+            }
+        ]
+    }
+
+    oHttp.send(JSON.stringify(data));    
+}
+
+
+function genere_questions(temperature, nouveau_theme, bouton) {    
+
+    afficheVue("generation_questions_en_cours"); // si on reregénère les questions
+    afficheVue("pageListeQuestions");
+ 
+    var oHttp = new XMLHttpRequest();
+    oHttp.open("POST", url_GPT);
+    oHttp.setRequestHeader("Accept", "application/json");
+    oHttp.setRequestHeader("Content-Type", "application/json");
+    oHttp.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY)
+
+    oHttp.onreadystatechange = function () {
+        if (oHttp.readyState === 4) {
+                    
+            var oJson = {};
+            try {
+                oJson = JSON.parse(oHttp.responseText);
+            } catch (ex) {
+                console.log("Error: " + ex.message);
+            }
+            oJson = JSON.parse(oHttp.responseText);
+            //console.log(oJson);
+            var reponseGPT_str = oJson.choices[0].message.content;            
+            var reponseGPT_liste = reponseGPT_str.split(/\r?\n/).filter(element => element);
+            affiche_questions(reponseGPT_liste, nouveau_theme);
+        }
+    }
+
+    var resumeCours = document.getElementById("resumeCours").value;
+
+    var fin_prompt = "Complète la liste de questions à trous-ci dessous en utilisant les idées principales du cours dans un ordre quelconque.\n\n";
+    //var fin_prompt = "Complète la liste de questions à trous-ci dessous en utilisant les idées principales du cours de manière la plus pertinente possible.\n\n";
+    listeQuestionsTypes.forEach(function(texte_question) {
+        fin_prompt += texte_question + "\n";
+    });
+    fin_prompt += "\n\nUtilise une ligne par question et commence celle-ci directement par la question sans utiliser de numérotation ni de tirets."    
+    var prompt = "Voici les idées principales d'un cours : \n\n" + resumeCours + "\n\n" + fin_prompt;
+    //console.log(prompt);
+
+    data = {
+        model: model_GPT,
+        temperature: temperature,
+        messages: [
+            {
+                "role": "system",
+                "content": "Tu es un enseignant qui fait réfléchir en profondeur aux concepts que tu enseignes."
+            },
+            {
+                "role": "user", //system,user,assistant
+                "content": prompt
+            }
+        ]
+    }
+
+    oHttp.send(JSON.stringify(data));
+}
+
+
+function supprime_Question(i) {
+    listeQuestions.splice(i, 1);
+    affiche_questions(listeQuestions, true);
+}
+
+
+function genere_reponses() {
+    afficheVue("pageListeReponses");
+
+    var oHttp = new XMLHttpRequest();
+    oHttp.open("POST", url_GPT);
+    oHttp.setRequestHeader("Accept", "application/json");
+    oHttp.setRequestHeader("Content-Type", "application/json");
+    oHttp.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY)
+
+    oHttp.onreadystatechange = function () {
+        if (oHttp.readyState === 4) {                    
+            var oJson = {};
+            try {
+                oJson = JSON.parse(oHttp.responseText);
+            } catch (ex) {
+                console.log("Error: " + ex.message);
+            }
+            oJson = JSON.parse(oHttp.responseText);
+            //console.log(oJson);
+            var reponseGPT_str = oJson.choices[0].message.content;
+            //console.log(reponseGPT_str);
+
+            // résoud le bug de la "," après le dernier element du tableau            
+            reponseGPT_str = reponseGPT_str.replace(",\n]","]");            
+
+            try {
+                listeQuestionsReponses = JSON.parse(reponseGPT_str);
+            } catch(ex) {
+                console.log("Erreur : " + reponseGPT_str);
+            }
+
+            affiche_reponse_I(0);
+        }
+    }
+
+    var prompt = "Voici les idées principales d'un cours : \n\n";
+    prompt += document.getElementById("resumeCours").value + "\n\n";
+    prompt += "Voici un fichier JSON qui contient des questions. Complète le champ réponse pour chacune des questions en utilisant les idées principales utilisées dans le cours.\n\n";
+    
+    var listeJSON = "[\n";
+    listeQuestions.forEach(function(question, index) {
+        let element = {question : question, reponse : ""}
+        let fin_chaine = (index === listeQuestions.length - 1) ? "\n" : ",\n";
+        listeJSON += JSON.stringify(element) + fin_chaine;
+    });
+    listeJSON += "]"
+    
+    prompt += listeJSON;
+    //console.log(prompt);
+
+    data = {        
+        model: model_GPT,
+        temperature: 0.7,
+        messages: [
+            {
+                "role": "system",
+                "content": "Tu es un enseignant qui fait réfléchir en profondeur aux concepts que tu enseignes."
+            },
+            {
+                "role": "user", //system,user,assistant
+                "content": prompt
+            }
+        ]
+    }
+
+    oHttp.send(JSON.stringify(data));
+}
+
---
+++ scripts/gestionPage.js
@@ -0,0 +1,204 @@
+//--------------------------------------------------------------------------------------//
+// variables globales
+//--------------------------------------------------------------------------------------//
+// initialisation
+
+function initialisePage() { 
+	// initialise l'affichage des vues
+	initialiseVues();
+	
+	// affichage des éléments particuliers
+	//affiche_questions(listeQuestionsTypes, true); 
+}
+
+
+//--------------------------------------------------------------------------------------//
+function affiche_questions(nouvelles_questions, nouveau_theme) {
+    // nouvelles_questions.forEach(function(question, index) {
+    //     console.log("Question " + (index+1) + " : " + question);
+    // });
+
+    var tableau_liste_questions = document.getElementById("tableQuestions");
+
+    if(nouveau_theme) {
+        // on efface tout le tableau
+        while (tableau_liste_questions.firstChild) {
+            tableau_liste_questions.removeChild(tableau_liste_questions.firstChild);
+        }
+
+        listeQuestions = nouvelles_questions;
+    } else {
+        listeQuestions = listeQuestions.concat(nouvelles_questions);
+    }
+
+    var tblBody = document.createElement("tbody");
+    tblBody.setAttribute("id", "corps_table_liste_questions");
+
+    // pour toutes les questions
+    nouvelles_questions.forEach(function(question, index) {	
+
+        var ligne = document.createElement("tr");
+
+        var cellule_question = document.createElement("td");
+        var texte_cellule_question = document.createTextNode(question);
+        cellule_question.appendChild(texte_cellule_question);
+        ligne.appendChild(cellule_question);
+
+        var cellule_edit = document.createElement("td");
+        cellule_edit.innerHTML = '<i class="fas fa-pen"></i>';
+        cellule_edit.setAttribute("onclick", "edit_question(" + index + ");");
+        ligne.appendChild(cellule_edit);
+
+        var cellule_delete = document.createElement("td");
+        cellule_delete.innerHTML = '<i class="fas fa-trash"></i>';
+        cellule_delete.setAttribute("onclick", "supprime_Question(" + index + ");");
+        ligne.appendChild(cellule_delete);
+    
+        tblBody.appendChild(ligne);
+
+    });
+
+    tableau_liste_questions.appendChild(tblBody);
+
+    afficheVue("pageValidationQuestion");    
+}
+
+//--------------------------------------------------------------------------------------//
+function edit_question(i) {    
+    const corps_table = document.getElementById("corps_table_liste_questions");
+    let index = 0;
+    for (const ligne of corps_table.children) {        
+
+        if(index == i) {
+            // on enleve tout le contenu
+            while(ligne.firstChild) {
+                ligne.removeChild(ligne.firstChild);
+            }
+
+            // texte editable
+            var cellule_question = document.createElement("td");
+            var texte_editable = document.createElement("input");
+            texte_editable.setAttribute("id","texte_editable_modif_question");
+            texte_editable.setAttribute("type", "text");
+            texte_editable.setAttribute("class", "u-full-with");
+            texte_editable.setAttribute("size",60)
+            texte_editable.value = listeQuestions[i];
+            cellule_question.appendChild(texte_editable);
+            ligne.appendChild(cellule_question);
+
+            var cellule_valide = document.createElement("td");
+            cellule_valide.innerHTML = '<i class="fas fa-check"></i>';
+            cellule_valide.setAttribute("onclick", "modifie_question(" + index + ");");
+            ligne.appendChild(cellule_valide);
+    
+            var cellule_cancel = document.createElement("td");
+            cellule_cancel.innerHTML = '<i class="fas fa-times"></i>';
+            cellule_cancel.setAttribute("onclick", "termine_modifications_question(" + index + ");");
+            ligne.appendChild(cellule_cancel);    
+        }
+
+        index++;
+    }
+}
+
+function modifie_question(i) {
+    listeQuestions[i] = document.getElementById("texte_editable_modif_question").value;
+    termine_modifications_question(i);
+}
+
+//--------------------------------------------------------------------------------------//
+function termine_modifications_question(i) {    
+    const corps_table = document.getElementById("corps_table_liste_questions");
+    let index = 0;
+    for (const ligne of corps_table.children) {        
+
+        if(index == i) {
+            // on enleve tout le contenu
+            while(ligne.firstChild) {
+                ligne.removeChild(ligne.firstChild);
+            }
+
+            // texte editable
+            var cellule_question = document.createElement("td");
+            var texte_cellule_question = document.createTextNode(listeQuestions[i]);
+            cellule_question.appendChild(texte_cellule_question);
+            ligne.appendChild(cellule_question);
+
+            var cellule_edit = document.createElement("td");
+            cellule_edit.innerHTML = '<i class="fas fa-pen"></i>';
+            cellule_edit.setAttribute("onclick", "edit_question(" + index + ");");
+            ligne.appendChild(cellule_edit);
+    
+            var cellule_delete = document.createElement("td");
+            cellule_delete.innerHTML = '<i class="fas fa-trash"></i>';
+            cellule_delete.setAttribute("onclick", "supprime_Question(" + index + ");");
+            ligne.appendChild(cellule_delete);    
+        }
+
+        index++;
+    }
+}
+
+
+//--------------------------------------------------------------------------------------//
+function affiche_reponse_I(i) {
+    document.getElementById("numeroQuestion").textContent = i+1;
+	document.getElementById("nombreQuestions").textContent = listeQuestionsReponses.length;
+    document.getElementById("texteQuestion").textContent = listeQuestionsReponses[i].question;
+    document.getElementById("reponseQuestion").value = listeQuestionsReponses[i].reponse;	
+	document.getElementById("btn_question_suivante").setAttribute("onclick", "valide_reponse_I(" + i + ");");
+
+	var derniere_question = (i === (listeQuestionsReponses.length-1));
+	if(derniere_question) {
+        document.getElementById("btn_question_suivante").setAttribute("class", "button-success u-full-width");
+		document.getElementById("btn_question_suivante").innerHTML = '<i class="fas fa-check"></i> Terminer';
+	}
+
+    afficheVue("pageValidationReponses");
+}
+
+//--------------------------------------------------------------------------------------//
+function valide_reponse_I(i) {
+	// on enregristre les modifications
+	listeQuestionsReponses[i].reponse = document.getElementById("reponseQuestion").value;
+
+	// on passe à la question suivante
+	var derniere_question = (i === (listeQuestionsReponses.length-1));
+	if(derniere_question) {
+		affiche_page_bilan();
+	} else {
+		affiche_reponse_I(i+1);
+	}
+}
+
+//--------------------------------------------------------------------------------------//
+function affiche_page_bilan() {
+    var div_liste_QR = document.getElementById("divListeQR");
+    
+    // on efface tout le tableau
+	while (div_liste_QR.firstChild) {
+		div_liste_QR.removeChild(div_liste_QR.firstChild);
+	}
+    
+    // pour toutes les questions
+    listeQuestionsReponses.forEach(function(QR) {	
+
+        var paragraphe1 = document.createElement("p");
+		paragraphe1.innerHTML = "<strong>Question :</strong> " + QR.question;
+        div_liste_QR.appendChild(paragraphe1);
+
+		var paragraphe2 = document.createElement("p");
+		paragraphe2.innerHTML = "<strong>Réponse :</strong>";
+        div_liste_QR.appendChild(paragraphe2);
+
+		var paragraphe3 = document.createElement("p");
+		paragraphe3.innerHTML = QR.reponse;
+		div_liste_QR.appendChild(paragraphe3);
+
+		var element_hr = document.createElement("hr");
+		div_liste_QR.appendChild(element_hr);
+
+    });    
+
+	afficheVue("pageSynthese");
+}
